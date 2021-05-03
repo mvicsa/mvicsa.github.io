1f15f0bebcdf0be938ac03acbf7348efe4b42634
mvicsa <mvicsa@yahoo.com>
2021-05-03 10:25:09 +0200

    Create switcher.js

---
+++ js/switcher.js
@@ -0,0 +1,31 @@
+const colorStyle = document.getElementById('color'),
+      switcher = document.querySelector('.switcher'),
+      switchSetting = document.querySelector('.switcher .settings'),
+      switchColors = document.querySelector('.switcher .switcher-colors');
+
+
+switchSetting.onclick = () => {
+
+  switcher.classList.toggle('active');
+
+};
+
+switchColors.children.forEach((el) => {
+
+  let color = el.children[0].className;
+
+  el.onclick = () => {
+
+
+    colorStyle.href = `css/colors/${color}.css`;
+    document.cookie = `color=${color};expires=31 Dec 2025 23:59:59`;
+    
+  };
+
+  if (document.cookie.indexOf(`${color}`) != -1) {
+    
+    colorStyle.href = `css/colors/${color}.css`;
+
+  }
+
+});
